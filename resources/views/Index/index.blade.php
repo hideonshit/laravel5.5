b9c85a67a59a59d4cf5b67ba093710c17bcf367e
hideonshit <755585012@qq.com>
2017-10-08 08:54:47 +0800

    修改首页文件

--- resources/views/Index/index.blade.php
+++ resources/views/Index/index.blade.php
@@ -5,7 +5,7 @@
 	<link rel="stylesheet" href="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="http://cdn.static.runoob.com/libs/jquery/2.1.1/jquery.min.js"></script>
 	<script src="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<title>Document</title>
+	<title>为所欲为</title>
 </head>
 <body>
 	<nav class="navbar navbar-default navbar-fixed-top" role="navigation"> 
